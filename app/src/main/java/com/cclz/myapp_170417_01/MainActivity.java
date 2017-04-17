94508be1a71d856d0f638f5c049fd97e0d28f0b2
Cclz <rex620620>
2017-04-17 14:01:00 +0800

    Initial commit

---
+++ app/src/main/java/com/cclz/myapp_170417_01/MainActivity.java
@@ -0,0 +1,44 @@
+package com.cclz.myapp_170417_01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+
+    /* volley
+       1. 建立一個 queue
+       2. 建立一個 request ( a. 成功了怎麼辦  b. 失敗了怎麼辦)
+       3. 把 z 加入 1
+       4. 叫 queue 跑
+    */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        RequestQueue queue= Volley.newRequestQueue(MainActivity.this);
+        StringRequest request=new StringRequest("http://data.ntpc.gov.tw/od/data/api/BF90FA7E-C358-4CDA-B579-B6C84ADC96A1?$format=json",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("NET", response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        queue.add(request);
+        queue.start();
+
+
+
+    }
+}
