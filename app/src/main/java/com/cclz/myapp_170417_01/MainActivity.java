354bef8fa4d5086ccae574a598f9f26ba1226230
Cclz <rex620620>
2017-04-17 14:27:29 +0800

    Get JSON

--- app/src/main/java/com/cclz/myapp_170417_01/MainActivity.java
+++ app/src/main/java/com/cclz/myapp_170417_01/MainActivity.java
@@ -10,13 +10,21 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
-    /* volley
+   /* volley
        1. 建立一個 queue
        2. 建立一個 request ( a. 成功了怎麼辦  b. 失敗了怎麼辦)
        3. 把 z 加入 1
        4. 叫 queue 跑
+
+       JSON
+       [] 陣列
+       {} 物件
     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +36,16 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(String response) {
                         Log.d("NET", response);
+                        try {
+                            JSONArray array=new JSONArray(response);
+                            for(int i=0; i<array.length(); i++){
+                                JSONObject obj=array.getJSONObject(i);
+                                String s=obj.getString("district");
+                                Log.d("NET", s);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }, new Response.ErrorListener() {
             @Override
